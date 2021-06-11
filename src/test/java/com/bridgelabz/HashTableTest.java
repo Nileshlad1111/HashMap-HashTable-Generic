af7294393fae3ca5df7c15658fb99a5d03693f38
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-06-11 16:35:11 +0530

    ADDED:To remove Avoidable_Word

--- src/test/java/com/bridgelabz/HashTableTest.java
+++ src/test/java/com/bridgelabz/HashTableTest.java
@@ -21,7 +21,6 @@ public class HashTableTest {
             myHashMap.add(word, value);
         }
         int frequency = myHashMap.get("to");
-        myHashMap.printMyMap();
         Assert.assertEquals(2, frequency);
     }
 
@@ -45,5 +44,24 @@ public class HashTableTest {
         }
         int frequency = myHashMap.get("paranoid");
         Assert.assertEquals(4, frequency);
+    }
+
+    @Test
+    public void provideWord_WhenRemovedFromHashMap_TestCaseShouldPass()
+    {
+        String sentence = "paranoid are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situation";
+        MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
+        String [] words = sentence.toLowerCase().split(" ");
+        for (String word : words) {
+            Integer value = myHashMap.get(word);
+            if (value == null) {
+                value = 1;
+            } else {
+                value = value + 1;
+            }
+            myHashMap.add(word, value);
+        }
+        int isValueRemoved = myHashMap.delete("avoidable");
+        Assert.assertEquals(1, isValueRemoved);
     }
 }
