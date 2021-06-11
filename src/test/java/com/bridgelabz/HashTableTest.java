d06b3783086bcf48ac5724142b26bc641f735a3f
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-06-11 16:08:11 +0530

    ADDED:To find frequency in words

--- src/test/java/com/bridgelabz/HashTableTest.java
+++ src/test/java/com/bridgelabz/HashTableTest.java
@@ -1,7 +1,27 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class HashTableTest {
-    public static void main(String[] args) {
-        System.out.println("welcome map using hashtable project problem");
+    @Test
+    public void provideSentence_WhenWordAddedToList_ShouldReturnFrequency()
+    {
+        String sentence = "to be or not to be";
+        MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
+        String [] words = sentence.toLowerCase().split(" ");
+        for (String word : words){
+            Integer value = myHashMap.get(word);
+            if(value == null) {
+                value = 1;
+            }
+            else {
+                value = value + 1;
+            }
+            myHashMap.add(word, value);
+        }
+        int frequency = myHashMap.get("to");
+        myHashMap.printMyMap();
+        Assert.assertEquals(2, frequency);
     }
 }
